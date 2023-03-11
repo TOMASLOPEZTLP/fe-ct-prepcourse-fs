f84c97d678cce93444e0a16b0e332b0a2ff7b501
Tomas Lopez <tl88245@gmail.com>
2023-03-11 09:14:29 +0100

    07

--- M07 JavaScript Clases/Ejercicio 02.js
+++ M07 JavaScript Clases/Ejercicio 02.js
@@ -6,13 +6,33 @@ class Persona {
    // Debe tener las propiedades: "nombre", "apellido", "edad" y "domicilio".
    // Debe tener un método llamado "detalle" que nos devuelva un objeto con las propiedades de la persona y
    // sus valores.
+
+   constructor( nombre, apellido, edad, domicilio ){
+      this.nombre= nombre;
+      this.apellido= apellido;
+      this.edad= edad; 
+      this.domicilio=domicilio;
+      }
+
+      detelle() {
+         return{
+            nombre : this.nombre,
+            apellido : this.apellido,
+            edad : this.edad,
+            domicilio : this.domicilio,
+         }
+      }
+
 }
+
 
 function crearInstanciaPersona(nombre, apellido, edad, domicilio) {
    // En este ejercicio debes crear una instancia de la clase construida en el ejercicio anterior.
    // Recibirás las propiedades por parámetro.
    // Retornar la instancia creada.
    // Tu código:
+
+      return new Persona(nombre, apellido, edad, domicilio)
 }
 
 function agregarMetodo() {
